a51ae7bdf3bf3754fe7d738e9618779875df0d2f
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-02-26 11:31:01 +0700

    Handling input text

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,37 @@
 import { ref } from "vue";
 
 const showModal = ref(false);
+const newNote = ref("");
+const notes = ref([]);
+
+function getRandomColor(){
+  return "hsl("+ Math.random() * 360 +", 100%, 75%)";
+}
+
+const addNote = () => {
+  notes.value.push({
+    id: Math.floor(Math.random() * 1234556),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+  showModal.value = false;
+  newNote.value = "";
+};
+
 </script>
 <template>
   <main>
     <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
+        <textarea
+          v-model="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button @click="addNote">Add Note</button>
         <button class="close" @click="showModal = false">Close</button>
       </div>
     </div>
@@ -18,12 +42,21 @@ const showModal = ref(false);
         <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
+        {{ notes }}
         <div class="card">
-          <p class="main-text">Hidup ini adalah PERJALANAN PANJANG, dan sebagaimana sabda Nabi -shollallohu alaihi wasallam-:  “Perjalanan panjang adalah potongan dari adzab..” [HR. Bukhori dan Muslim]</p>
+          <p class="main-text">
+            Hidup ini adalah PERJALANAN PANJANG, dan sebagaimana sabda Nabi
+            -shollallohu alaihi wasallam-:  “Perjalanan panjang adalah potongan
+            dari adzab..” [HR. Bukhori dan Muslim]
+          </p>
           <p class="date">04/11/2024</p>
         </div>
         <div class="card">
-          <p class="main-text">Hidup ini adalah PERJALANAN PANJANG, dan sebagaimana sabda Nabi -shollallohu alaihi wasallam-:  “Perjalanan panjang adalah potongan dari adzab..” [HR. Bukhori dan Muslim]</p>
+          <p class="main-text">
+            Hidup ini adalah PERJALANAN PANJANG, dan sebagaimana sabda Nabi
+            -shollallohu alaihi wasallam-:  “Perjalanan panjang adalah potongan
+            dari adzab..” [HR. Bukhori dan Muslim]
+          </p>
           <p class="date">04/11/2024</p>
         </div>
       </div>
@@ -32,99 +65,98 @@ const showModal = ref(false);
 </template>
 
 <style scoped>
-  main{
-    height: 100vh;
-    width: 100vw;
-  }
+main {
+  height: 100vh;
+  width: 100vw;
+}
 
-  .container{
-    min-width: 1000px;
-    padding: 10px;
-    margin: 0 auto;
-  }
+.container {
+  min-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
 
-  header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  h1{
-    font-weight: bold;
-    margin-bottom: 25px;
-    font-size: 75px;
-  }
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
 
-  header button{
-    bottom: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    background-color: rgb(21,20,20);
-    border-radius: 100%;
-    color: white;
-    font-size: 20px;
-  }
+header button {
+  bottom: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+}
 
-  .card{
-    width: 225px;
-    height: 225px;
-    background-color: rgb(237,182,44);
-    padding: 10px;
-    border-radius:15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-right: 20px;
-    margin-left: 20px;
-  }
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-left: 20px;
+}
 
-  .date{
-    font-size: 11px;
-    font-weight: bold;
-  }
+.date {
+  font-size: 11px;
+  font-weight: bold;
+}
 
-  .cards-container {
-    display: flex;
-    flex-wrap: wrap;
-  }
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
 
-  .overlay{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.77);
-    z-index: 10;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .modal{
-    width: 750px;
-    background-color: #fff;
-    border-radius: 10px;
-    padding: 30px;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-  }
+.modal {
+  width: 750px;
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
 
-  .modal button {
-    padding: 10px 20px;
-    font-size: 20px;
-    width: 100%;
-    background-color: blueviolet;
-    border: none;
-    color: white;
-    cursor: pointer;
-    margin-top: 15px;
-  }
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
 
-  .modal .close {
-    background-color: rgb(193, 15, 15);
-    margin-top: 7px;
-  }
-
+.modal .close {
+  background-color: rgb(193, 15, 15);
+  margin-top: 7px;
+}
 </style>
